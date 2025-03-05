363714ff5e86a2cbc054040d89fa9a298d8c25c1
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-03-05 11:36:30 +0100

    clear project and create first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <header>
+      <div>Logo</div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Profile</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default App
+const Section = () => {
+  return (
+    <section>
+      <h2>Welcome to React</h2>
+      <button>click</button>
+      <button>Hello</button>
+      <button>submit</button>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id
+        reprehenderit exercitationem nulla magni, nostrum minima sunt autem,
+        molestias tempore quod necessitatibus perferendis ullam voluptatum
+        laudantium sequi repellat at saepe quas?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus vero
+        quasi error iste deserunt veritatis, dignissimos voluptate, tenetur
+        illum unde repellendus doloribus perspiciatis voluptates ipsum hic
+        consectetur quae eligendi dolorum.
+      </p>
+    </section>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>&copy; All right reserved</span>
+    </footer>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Section />
+      <Section />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
