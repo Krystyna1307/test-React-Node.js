0d28d00ff22593e38406bbf46b54fd577c5aba83
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-03-05 16:53:12 +0100

    icon

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,8 +1,14 @@
+import { TbAdjustments } from "react-icons/tb";
+import s from "./Message.module.css";
+
 const Message = ({ text, author = "Default author" }) => {
   return (
     <div>
       <h4>{text}</h4>
-      <span>{author}</span>
+      <span>
+        {author}
+        <TbAdjustments className={s.icon} />
+      </span>
     </div>
   );
 };
