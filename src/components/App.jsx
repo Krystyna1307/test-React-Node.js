a891948ae62c3b55bd7953fdcca299e0cd84e619
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-03-05 12:10:43 +0100

    props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Message from "./Message/Message";
 import Section from "./Section/Section";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Section />
-      <Section />
-      <Footer />
+      <Message text="Куплю праску!" author="Олена" />
+      <Message text="Продам!" />
     </div>
   );
 };
