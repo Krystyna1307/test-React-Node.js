e3fd9401fe9cb19864692c48bc4e0f7a3f521ef8
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-03-05 12:47:20 +0100

    mod1-les1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,42 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import List from "./List/List";
 import Message from "./Message/Message";
 import Section from "./Section/Section";
 
 const App = () => {
+  const isOnline = false;
+
+  const filmsData = [
+    {
+      id: "1",
+      title: "Terminator",
+    },
+    {
+      id: "2",
+      title: "Mr.Bean",
+    },
+  ];
+
+  const goodsData = [
+    {
+      id: "1",
+      title: "Морква",
+    },
+    {
+      id: "2",
+      title: "Капуста",
+    },
+  ];
+
   return (
     <div>
+      {isOnline && <h1>Welcome</h1>}
       <Header />
+
+      <List title="My films" data={filmsData} />
+      <List title="Goods" data={goodsData} />
+
       <Message text="Куплю праску!" author="Олена" />
       <Message text="Продам!" />
     </div>
---
+++ src/components/List/List.jsx
@@ -0,0 +1,14 @@
+const List = ({ data, title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
